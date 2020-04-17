01331f4755ea31214d25f5d5a1772d000e4126e8
noambenmoshe <b.noam@campus.technion.ac.il>
2020-04-17 15:57:37 +0300

    home with pic

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -4,13 +4,13 @@
     <f7-navbar :sliding="false" large>
       <f7-nav-left>
         <f7-link icon-ios="f7:menu" icon-aurora="f7:menu" icon-md="material:menu" panel-open="left"></f7-link>
-        <img src='coronaMEDTitle.jpg'>
       </f7-nav-left>
       <f7-nav-title sliding>CoronaMED</f7-nav-title>
       <!-- <f7-nav-right>
         <f7-link icon-ios="f7:menu" icon-aurora="f7:menu" icon-md="material:menu" panel-open="right"></f7-link>
       </f7-nav-right> -->
-      <f7-nav-title-large sliding>CoronaMED</f7-nav-title-large>
+      <!-- <f7-nav-title-large sliding>CoronaMED</f7-nav-title-large> -->
+      <img src='static/coronaMEDTitle.jpg' height="120" width="200">
     </f7-navbar>
     <!-- Toolbar-->
     <!-- <f7-toolbar bottom>
@@ -25,7 +25,7 @@
 
       <p>The fun thing is that you can easily control one view from another without any line of JavaScript just using "data-view" attribute on links.</p>
     </f7-block> -->
-
+    
     <f7-block-title>Navigation</f7-block-title>
     <f7-list>
       <f7-list-item link="/about/" title="About"></f7-list-item>
